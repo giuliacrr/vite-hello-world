0faf51228bd4c1cfffad41201d963d5062e95338
giuliacrr <giulia.corrado02@gmail.com>
2023-08-21 16:02:26 +0200

    Added title + image

---
+++ src/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+import HeroSec from "./components/AppHero.vue";
+export default {
+  components: {
+    HeroSec,
+  },
+};
+</script>
+
+<template>
+  <div class="bg-success p-3 text-center text-white">
+    <h1>Welcome to my first vite app!</h1>
+    <h4>(or whatever it should be?)</h4>
+    <h6>(this is a hero section)</h6>
+  </div>
+  <HeroSec></HeroSec>
+</template>
+
+<style></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,11 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <img class="w-100" src="../assets/beemo.jpg" alt="beemo" />
+  </div>
+</template>
+
+<style></style>
